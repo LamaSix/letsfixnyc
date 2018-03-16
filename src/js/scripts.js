027a168c168bb3eee0089513638f0090c8993c51
Max Kirchoff <max@lamasix.com>
2018-03-16 09:25:25 -0400

    updated twitter pixel

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -67,8 +67,11 @@
             $('#revmsg-click-to-call .success').html(data.message);
 
             $('#revmsg-CallSignupForm').hide();
+              var twitterPixel = "<script type=\"text/javascript\">twttr.conversion.trackPid('nz5eq', { tw_sale_amount: 0, tw_order_quantity: 0 });</script><noscript><img height=\"1\" width=\"1\" style=\"display:none;\" alt=\"\" src=\"https://analytics.twitter.com/i/adsct?txn_id=nz5eq&p_id=Twitter&tw_sale_amount=0&tw_order_quantity=0\" /><img height=\"1\" width=\"1\" style=\"display:none;\" alt=\"\" src=\"//t.co/i/adsct?txn_id=nz5eq&p_id=Twitter&tw_sale_amount=0&tw_order_quantity=0\" /></noscript>";
+              var facebookPixel = "<script>!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init', '555717268143031');fbq('track', 'PageView');</script><noscript><img height=\"1\" width=\"1\" src=\"https://www.facebook.com/tr?id=555717268143031&ev=PageView&noscript=1\"/></noscript>";
 
-            $('#revmsg-click-to-call .success').append("<script>!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init', '555717268143031');fbq('track', 'PageView');</script><noscript><img height=\"1\" width=\"1\" src=\"https://www.facebook.com/tr?id=555717268143031&ev=PageView&noscript=1\"/></noscript>");3
+              $('#revmsg-click-to-call .success').append(facebookPixel);
+              $('#revmsg-click-to-call .success').append(twitterPixel);
 
             $('#revmsg-click-to-call .success').show();
           }
